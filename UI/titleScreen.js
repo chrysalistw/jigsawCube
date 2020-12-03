0fe3baa3a89031acb7f22c5d52fcc6f293cae63b
lightfulhuang <lightfulhuang@gmail.com>
2020-12-03 13:05:01 +0800

    .

--- UI/titleScreen.js
+++ UI/titleScreen.js
@@ -2,7 +2,9 @@ import Screen from "./Screen.js"
 
 var titleScreen = new Screen()
 titleScreen.init = function(){
+	this.width = this.cvs.width
+	this.height = this.cvs.height
 	this.ctx.fillStyle = "#0FF"
-	this.ctx.fillRect(50,50,100,100)
+	this.ctx.fillRect(0,0,this.width,this.height)
 }
 export default titleScreen //???
--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 import titleScreen from "./UI/titleScreen.js"
 
 var cvs = document.getElementById("canvas")
+cvs.width = 500
+cvs.height = 500
 
 function main(){
 	titleScreen.applyTo(cvs)
