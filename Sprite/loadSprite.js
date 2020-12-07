35edf173a7596a79c180b44839bcf6d8a7281c49
lightfulhuang <lightfulhuang@gmail.com>
2020-12-07 08:16:32 +0800

    sprite

---
+++ Sprite/loadSprite.js
@@ -0,0 +1,10 @@
+import Sprite from "./Sprite.js"
+
+var spr = {}
+var loadSprites = async function(){
+	let test = await Sprite.loadSource("../pics/test.png")
+	spr.test = new Sprite(test, 0, 0, 442, 456)
+}
+
+export default loadSprites
+export { spr }
