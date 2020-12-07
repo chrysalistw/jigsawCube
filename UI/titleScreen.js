eafa03462ed59beba9ed3307d675d953e7495388
lightfulhuang <lightfulhuang@gmail.com>
2020-12-07 08:47:41 +0800

    dynamically load sprite

--- UI/titleScreen.js
+++ UI/titleScreen.js
@@ -1,6 +1,8 @@
 import Screen from "./Screen.js"
 import Button from "./Button.js"
 import gamingScreen from "./gamingScreen.js"
+//import loadSprite from "../Sprite/loadSprite.js"
+import { spr } from "../Sprite/loadSprite.js"
 
 var titleScreen = new Screen()
 titleScreen.view = function(){
@@ -8,6 +10,7 @@ titleScreen.view = function(){
 	this.height = this.cvs.height
 	this.ctx.fillStyle = "#0FF"
 	this.ctx.fillRect(0,0,this.width,this.height)
+	spr.test.draw(this.ctx, 30, 30)
 }
 titleScreen.addFeatures = function(){
 	let button = new Button(100,100,100,50, function(){
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import titleScreen from "./UI/titleScreen.js"
 import gamingScreen from "./UI/gamingScreen.js"
+import loadSprite from "./Sprite/loadSprite.js"
 
 var cvs = document.getElementById("canvas")
 cvs.width = 500
 cvs.height = 500
 
-function main(){
+async function main(){
+	await loadSprite()
 	titleScreen.applyTo(cvs)
 	titleScreen.init()
 	gamingScreen.applyTo(cvs)
