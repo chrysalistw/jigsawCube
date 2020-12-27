c4b919d4c8b83f0ab9df6679015f78672a8192eb
chrysalistw <lightfulhuang@gmail.com>
2020-12-27 20:47:44 +0800

    settingScreen layout draft

--- Sprite/loadSprite.js
+++ Sprite/loadSprite.js
@@ -19,6 +19,22 @@ var loadSprites = async function(){
 		await Sprite.loadSource("pics/settings.png"),
 		0, 0, 251, 49
 	)
+	spr.backgroundColor = new Sprite(
+		await Sprite.loadSource("pics/background_color.png"),
+		0, 0, 455, 45
+	)
+	spr.level = new Sprite(
+		await Sprite.loadSource("pics/level.png"),
+		0, 0, 155, 45
+	)
+	spr.tileSize = new Sprite(
+		await Sprite.loadSource("pics/tile_size.png"),
+		0, 0, 275, 45
+	)
+	spr.goBack = new Sprite(
+		await Sprite.loadSource("pics/go_back.png"),
+		0, 0, 215, 45
+	)
 	for(let level in levelStat){
 		let l = levelStat[level]
 		let w = l.width
--- UI/settingScreen.js
+++ UI/settingScreen.js
@@ -1,6 +1,7 @@
 import Screen from "./Screen.js"
 import Button from "./Button.js"
 import titleScreen from "./titleScreen.js"
+import { spr } from "../Sprite/loadSprite.js"
 
 var settingScreen = new Screen()
 var ss = settingScreen
@@ -8,54 +9,58 @@ var ss = settingScreen
 ss.view = function(){
 	this.ctx.fillStyle = sessionStorage.bgColor||"#0FC"
 	this.ctx.fillRect(0, 0, this.cvs.width, this.cvs.height)
+	spr.backgroundColor.draw(this.ctx, 10, 10)
+	spr.tileSize.draw(this.ctx, 10, 125)
+	spr.level.draw(this.ctx, 10, 400)
 }
 ss.addFeatures = function(){
-	//go back to title
-	new Button(0, 0, 50, 25, e=>{
-		ss.kill()
-		titleScreen.init()
-	}).attachView(
-		ss.strokedSquare("red", 0, 0, 50, 25)
-	).applyTo(ss)
-	new Button(60,10,50,50, e=>{
+	//bg color
+	new Button(70,65,50,50, e=>{
 		sessionStorage.setItem("bgColor", "blue")
 		ss.reboot()
 	}).attachView(
-		ss.strokedSquare("blue", 60, 10, 50, 50)
+		ss.strokedSquare("blue", 70, 65, 50, 50)
 	).applyTo(ss)
-	new Button(120,10,50,50, e=>{
+	new Button(130,65,50,50, e=>{
 		sessionStorage.setItem("bgColor", "#0FC")
 		ss.reboot()
 	}).attachView(
-		ss.strokedSquare("#0FC", 120, 10, 50, 50)
+		ss.strokedSquare("#0FC", 130, 65, 50, 50)
 	).applyTo(ss)
-	new Button(180,10,50,50, e=>{
+	new Button(190,65,50,50, e=>{
 		sessionStorage.setItem("bgColor", "orange")
 		ss.reboot()
 	}).attachView(
-		ss.strokedSquare("orange", 180, 10, 50, 50)
+		ss.strokedSquare("orange", 190, 65, 50, 50)
 	).applyTo(ss)
 
 	//choose level
-	new Button(10,80,50,50, e=>{
+	new Button(10,455,50,50, e=>{
 		sessionStorage.setItem("level", "3x3")
 	}).attachView(
-		ss.strokedSquare("#ccc", 10, 80, 50, 50)
+		ss.strokedSquare("#ccc", 10, 455, 50, 50)
 	).applyTo(ss)
-	new Button(70,80,50,50, e=>{
+	new Button(70,455,50,50, e=>{
 		sessionStorage.setItem("level", "4x4")
 	}).attachView(
-		ss.strokedSquare("#eee", 70, 80, 50, 50)
+		ss.strokedSquare("#eee", 70, 455, 50, 50)
 	).applyTo(ss)
-	new Button(130,80,50,50, e=>{
+	new Button(130,455,50,50, e=>{
 		sessionStorage.setItem("level", "bubble")
 	}).attachView(
-		ss.strokedSquare("#800", 130, 80, 50, 50)
+		ss.strokedSquare("#800", 130, 455, 50, 50)
 	).applyTo(ss)
-	new Button(190,80,50,50, e=>{
+	new Button(190,455,50,50, e=>{
 		sessionStorage.setItem("level", "geometry")
 	}).attachView(
-		ss.strokedSquare("#FFF", 190, 80, 50, 50)
+		ss.strokedSquare("#FFF", 190, 455, 50, 50)
+	).applyTo(ss)
+	//go back
+	new Button(10, 545, 215, 45, e=>{
+		ss.kill()
+		titleScreen.init()
+	}).attachView(
+		()=>{spr.goBack.draw(this.ctx, 10, 545)}
 	).applyTo(ss)
 }
 ss.strokedSquare = function(color, x, y, h, w){
