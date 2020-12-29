6551ad1cfe861140b77bc7ad5a3c2197197602ab
lightfulhuang <lightfulhuang@gmail.com>
2020-12-30 06:21:35 +0800

    1.0

--- Sprite/loadSprite.js
+++ Sprite/loadSprite.js
@@ -15,18 +15,6 @@ var loadSprites = async function(){
 		await Sprite.loadSource("pics/level.png"),
 		0, 0, 161, 49
 	)
-	spr.newGame = new Sprite(
-		await Sprite.loadSource("pics/new_game.png"),
-		0, 0, 251, 49
-	)
-	spr.continue = new Sprite(
-		await Sprite.loadSource("pics/continue.png"),
-		0, 0, 251, 49
-	)
-	spr.settings = new Sprite(
-		await Sprite.loadSource("pics/settings.png"),
-		0, 0, 251, 49
-	)
 	spr.shuffle = new Sprite(
 		await Sprite.loadSource("pics/shuffle.png"),
 		0, 0, 215, 45
@@ -38,30 +26,6 @@ var loadSprites = async function(){
 	spr.chooseLevel = new Sprite(
 		await Sprite.loadSource("pics/choose_level.png"),
 		0, 0, 365, 45
-	)
-	spr.backgroundColor = new Sprite(
-		await Sprite.loadSource("pics/background_color.png"),
-		0, 0, 455, 45
-	)
-	spr.level = new Sprite(
-		await Sprite.loadSource("pics/level.png"),
-		0, 0, 161, 49
-	)
-	spr.tileSize = new Sprite(
-		await Sprite.loadSource("pics/tile_size.png"),
-		0, 0, 275, 45
-	)
-	spr.controller = new Sprite(
-		await Sprite.loadSource("pics/controller.png"),
-		0, 0, 305, 45
-	)
-	spr.drag = new Sprite(
-		await Sprite.loadSource("pics/drag.png"),
-		0, 0, 125, 45
-	)
-	spr.button = new Sprite(
-		await Sprite.loadSource("pics/button.png"),
-		0, 0, 185, 45
 	)
 	spr.goBack = new Sprite(
 		await Sprite.loadSource("pics/go_back.png"),
--- main.js
+++ main.js
@@ -1,30 +1,17 @@
 import titleScreen from "./UI/titleScreen.js"
 import gamingScreen from "./UI/gamingScreen.js"
-import settingScreen from "./UI/settingScreen.js"
 import levelScreen from "./UI/levelScreen.js"
 import loadSprite from "./Sprite/loadSprite.js"
-import Game from "./game.js"
 
-import { spr } from "./Sprite/loadSprite.js"
 var cvs = document.getElementById("canvas")
 cvs.width = 600
 cvs.height = 600
 
 async function main(){
-	//show splash / loading screen
 	await loadSprite()
-//	sessionStorage.setItem("bgColor", "#0FC")
-//	sessionStorage.setItem("level", "3x3")
 	titleScreen.applyTo(cvs)
-
 	gamingScreen.applyTo(cvs)
-//	let g = new Game("s", 3, 3)
-//	g.setField()
-
-//	gamingScreen.setGame(g)
-
 	levelScreen.applyTo(cvs)
-	settingScreen.applyTo(cvs)
 
 	titleScreen.init()
 }
